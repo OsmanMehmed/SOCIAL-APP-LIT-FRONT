4fd547c788102eba27b9aef736cd2272546585b4
Osman Mehmed <osman@devanddel.com>
2026-01-12 16:45:23 +0100

    deleting unused imports + deleting git references

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,4 @@ import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.j
 setBasePath("/shoelace");
 
 import "@shoelace-style/shoelace/dist/themes/light.css";
-
-import "@shoelace-style/shoelace/dist/components/button/button.js";
-import "@shoelace-style/shoelace/dist/components/input/input.js";
 import "@shoelace-style/shoelace/dist/components/icon/icon.js";
